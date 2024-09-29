9eb879d8ed5c41050bb96bef051f052d652a3eed
Vergil <126393669+mrd010@users.noreply.github.com>
2024-09-29 23:50:33 +0330

    Fix login route missing controller

--- server/src/routers/authRouter.ts
+++ server/src/routers/authRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { signup } from '../controllers/authController';
+import { login, signup } from '../controllers/authController';
 import { loginFormValidator, signupFormValidator } from '../middlewares/validators/authValidators';
 import { validationParser } from '../middlewares/validationParser';
 
 const authRouter = Router();
 
 authRouter.post('/signup', signupFormValidator, validationParser, signup);
-authRouter.post('/login', loginFormValidator, validationParser);
+authRouter.post('/login', loginFormValidator, validationParser, login);
 
 export default authRouter;
