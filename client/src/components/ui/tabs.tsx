2f92595add3a10c53a5db92a635e196b7b0fe1ef
Vergil <126393669+mrd010@users.noreply.github.com>
2024-10-07 23:57:04 +0330

    Create page tabs

--- client/src/components/ui/tabs.tsx
+++ client/src/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex h-full grow items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow-sm',
+      'inline-flex h-full w-full items-center justify-center gap-1 whitespace-nowrap px-3 py-1.5 text-sm font-semibold ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow-sm',
       className,
     )}
     {...props}
---
+++ client/src/pages/main/MainLayout.tsx
@@ -0,0 +1,48 @@
+import { CircleLoader1 } from '@/components/loaders';
+import { useAuth } from '@/hooks/context-hooks/useAuth';
+import { Navigate } from 'react-router-dom';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { LibraryBigIcon, UserIcon } from 'lucide-react';
+import Library from './Library';
+import Profile from './Profile';
+
+const MainLayout = () => {
+  const { status, user } = useAuth();
+
+  if (status !== 'authenticated') {
+    return <Navigate to="login" />;
+  }
+
+  if (!user) {
+    return (
+      <div className="grid size-full min-h-screen place-items-center">
+        <CircleLoader1 />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Tabs defaultValue="library">
+        <TabsList className="fixed bottom-0 left-0 w-full rounded-none border-t border-primary">
+          {/*  */}
+          <TabsTrigger value="library">
+            <LibraryBigIcon className="size-5" />
+            Library
+          </TabsTrigger>
+          <TabsTrigger value="profile">
+            <UserIcon className="size-5" />
+            Profile
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="library">
+          <Library />
+        </TabsContent>
+        <TabsContent value="profile">
+          <Profile />
+        </TabsContent>
+      </Tabs>
+    </>
+  );
+};
+export default MainLayout;
